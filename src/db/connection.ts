adc1df16c4ecd4ec72aec20e5e080c7b16fbd20d
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-06-01 16:46:17 -0500

    K

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,7 +4,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
 const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_HOST = process.env.DB_HOST || 'localhost';
-
+console.log(DB_HOST,DB_NAME,DB_PASSWORD,DB_NAME )
 
 const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
     host: DB_HOST,
