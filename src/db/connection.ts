396868f3e67df8e294396c2f10852343516a7d9d
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-06-06 19:38:24 -0500

    hola

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,22 +6,20 @@ const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
 const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_HOST = process.env.DB_HOST || 'localhost';
+const DATABASE_UR = process.env.DATABASE_URL || 'mysql://root:uHZQDJIQ5aNRtPdPgZAP@containers-us-west-134.railway.app:6143/railway';
 console.log(DB_HOST,DB_NAME,DB_PASSWORD,DB_NAME )
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
+const sequelize = new Sequelize( DATABASE_UR, {
+    // host: DB_HOST,
     dialect: 'mysql',
-    // dialectOptions: {
-    //     ssl: {
-    //       rejectUnauthorized: true
-    //     }
-    //   }
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+        
+      }
+    }
   });
   
 
 export default sequelize;
-// require('dotenv').config()
-// const mysql = require('mysql2')
-// export const sequelize = mysql.createConnection(process.env.DATABASE_URL)
-// console.log('Connected to PlanetScale!')
-// sequelize.end()
