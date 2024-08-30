bb50827279a5ddf7f9d5e8ba6b39d41075d9ae71
Juan Araujo Saucedo <juanjaraujo21@gmail.com>
2024-08-30 12:35:36 -0500

    CORRECION DE VARIABLES DE ENTORNO PARA PRODUCCIÓN

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,12 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
-const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DATABASE_UR = process.env.DATABASE_URL || 'mysql://root:uHZQDJIQ5aNRtPdPgZAP@containers-us-west-134.railway.app:6143/railway';
-console.log(DB_HOST, DB_NAME, DB_PASSWORD, DB_NAME);
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
+    throw new Error('Error al obtener credenciales de la DATABASE');
+}
 const sequelize = new sequelize_1.Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     // host: DB_HOST,
     dialect: 'mysql',
